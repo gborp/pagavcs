4600ff94381b5084185f5fcc35dd1044c3ee27ed
Gábor Pápai <gabor.papai@gmail.com>
2010-01-15 17:28:27 +0000

    add custom icons for messagepane

--- trunk/java/src/hu/pagavcs/gui/MessagePane.java
+++ trunk/java/src/hu/pagavcs/gui/MessagePane.java
@@ -7,6 +7,7 @@ import java.awt.Window;
 import java.awt.Dialog.ModalityType;
 import java.awt.event.ActionEvent;
 
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
@@ -15,7 +16,7 @@ import javax.swing.SwingConstants;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
-public class Message {
+public class MessagePane {
 
 	public enum OPTIONS {
 		OK, CANCEL
@@ -26,39 +27,42 @@ public class Message {
 	private final String message;
 	private OPTIONS      optionChoosed;
 	private JDialog      dialog;
+	private final Icon   icon;
 
 	public static OPTIONS showError(Window parent, String title, String message) {
-		return execute(parent, title, message);
+		return execute(parent, Manager.ICON_ERROR, title, message);
 	}
 
 	public static OPTIONS showWarning(Window parent, String title, String message) {
-		return execute(parent, title, message);
+		return execute(parent, Manager.ICON_WARNING, title, message);
 	}
 
 	public static OPTIONS showInfo(Window parent, String title, String message) {
-		return execute(parent, title, message);
+		return execute(parent, Manager.ICON_INFORMATION, title, message);
 	}
 
-	private static OPTIONS execute(Window parent, String title, String message) {
-		return new Message(parent, title, message).execute();
+	private static OPTIONS execute(Window parent, Icon icon, String title, String message) {
+		return new MessagePane(parent, icon, title, message).execute();
 	}
 
-	public Message(Window parent, String title, String message) {
+	public MessagePane(Window parent, Icon icon, String title, String message) {
 		this.parent = parent;
+		this.icon = icon;
 		this.title = title;
 		this.message = message;
 	}
 
 	public OPTIONS execute() {
-		JPanel panel = new JPanel(new FormLayout("p:g,p", "p,4dlu,p"));
+		JPanel panel = new JPanel(new FormLayout("p:g", "p:g,4dlu,p"));
 		CellConstraints cc = new CellConstraints();
 
 		Label lblMessage = new Label(message);
 		lblMessage.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMessage.setIcon(icon);
 		JButton btnOk = new JButton(new OkAction());
 
-		panel.add(lblMessage, cc.xywh(1, 1, 2, 1));
-		panel.add(btnOk, cc.xywh(2, 3, 1, 1));
+		panel.add(lblMessage, cc.xywh(1, 1, 1, 1));
+		panel.add(btnOk, cc.xywh(1, 3, 1, 1, CellConstraints.CENTER, CellConstraints.DEFAULT));
 
 		dialog = Manager.createDialog(parent, panel, title);
 		dialog.setModalityType(ModalityType.DOCUMENT_MODAL);
