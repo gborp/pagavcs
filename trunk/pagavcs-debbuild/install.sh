63e16413a0c92432aacb33806bc1e585cdfbac45
gabor.papai <gabor.papai@d68d8996-001d-11df-8113-359535dc4dea>
2010-02-13 08:13:10 +0000

    make deb repository ( deb http://pagavcs.googlecode.com/svn/ binary/ )

--- trunk/pagavcs-debbuild/install.sh
+++ trunk/pagavcs-debbuild/install.sh
@@ -5,4 +5,7 @@ cd ../..
 sudo dpkg -i `ls trunk/*.deb`
 mv -f trunk/*.deb binary
 dpkg-scanpackages binary /dev/null | gzip -9c > binary/Packages.gz
+cd binary
+apt-ftparchive release . > Release
+gpg -abs -o Release.gpg Release
 #nautilus -q
