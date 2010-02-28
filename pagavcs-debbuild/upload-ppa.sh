bc2741040155dc1cf2dd5bd2b55d7594b3611bd3
Gábor Pápai <gabor.papai@gmail.com>
2010-02-28 11:37:53 +0000

    ppa

--- pagavcs-debbuild/upload-ppa.sh
+++ pagavcs-debbuild/upload-ppa.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 cd ..
 dput ppa:gaborgabor/pagavcs *.changes
-rm *.dsc
-rm *.tar.gz
+rm --force *.dsc
+rm --force *.tar.gz
 #rm --force *.deb
-rm *.build
-rm *.changes
+rm --force *.build
+rm --force *.changes
 cat *.upload
-rm *.upload
+rm --force *.upload
