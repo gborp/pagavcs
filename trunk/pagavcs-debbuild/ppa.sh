b4ad402dcf0a36ee42a3191f3bfc98f81e58bece
gabor.papai <gabor.papai@d68d8996-001d-11df-8113-359535dc4dea>
2010-02-28 08:14:03 +0000

    ppa

--- trunk/pagavcs-debbuild/ppa.sh
+++ trunk/pagavcs-debbuild/ppa.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 cd ..
 sudo rm -r --force ppa-build
-rmdir ppa-build
-#cp -R pagavcs-debbuild/* ppa-build
 svn export pagavcs-debbuild ppa-build
 cd ppa-build
 ./build.sh
-debuild -S
+#debuild -S -k3FAEE1DA
