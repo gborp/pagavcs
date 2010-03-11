6c49ba852d9f2f42340c88574d033a67ea70ca9a
gabor.papai <gabor.papai@d68d8996-001d-11df-8113-359535dc4dea>
2010-03-11 19:55:24 +0000

    version 1.4.0 -- mugCommander is in the deb package

--- trunk/pagavcs-debbuild/build.sh
+++ trunk/pagavcs-debbuild/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-cd ../java
+cd ../../trunk-mugcommander/java
+./build.sh
+cd ../../trunk/java
 ./build.sh
 cd ../ppa-build
 rm -r debian/input/*
@@ -12,9 +14,11 @@ mkdir -p debian/input/icons/hicolor/scalable/actions
 mkdir -p debian/input/icons/hicolor/scalable/apps
 mkdir -p debian/input/icons/hicolor/scalable/emblems
 cp ../java/dist/pagavcs.jar debian/input/
+cp ../../trunk-mugcommander/java/dist/mugcommander.jar debian/input/
 cp ../java/dist/icon.png debian/input/
 cp ../scripts/pagavcs debian/input/
 cp ../scripts/pagavcs-nautilus.py debian/input/
+cp ../scripts/mugcommander debian/input/
 cp ../doc/* debian/input/doc
 cp ../icons/hicolor/scalable/actions/* debian/input/icons/hicolor/scalable/actions
 cp ../icons/hicolor/scalable/apps/* debian/input/icons/hicolor/scalable/apps
