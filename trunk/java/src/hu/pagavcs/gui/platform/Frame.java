8468551583f814121c5bfe9ae6147c23e3233a3e
Gábor Pápai <gabor.papai@gmail.com>
2010-04-25 05:49:08 +0000

    [delete gui] add Close button; all frames and dialogs are closable by pressing the Escape key

--- trunk/java/src/hu/pagavcs/gui/platform/Frame.java
+++ trunk/java/src/hu/pagavcs/gui/platform/Frame.java
@@ -1,20 +1,51 @@
 package hu.pagavcs.gui.platform;
 
 import hu.pagavcs.bl.Manager;
+import hu.pagavcs.bl.WindowPreferencesSaverOnClose;
 
+import java.awt.Rectangle;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.KeyStroke;
 
 public class Frame extends JFrame {
 
-	public Frame(String iconName) {
+	private final String applicationName;
+
+	public Frame(String applicationName, String iconName) {
 		super();
+		this.applicationName = applicationName;
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Manager.class.getResource(iconName)));
+
+		getRootPane().registerKeyboardAction(new ActionListener() {
+
+			public void actionPerformed(ActionEvent actionEvent) {
+				setVisible(false);
+			}
+		}, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);
 	}
 
 	public void setTitlePrefix(String prefix) {
 		setTitle(prefix + " - " + getTitle());
+	}
+
+	public void execute() {
+		pack();
+
+		Rectangle bounds = Manager.getSettings().getWindowBounds(applicationName);
+		if (bounds != null) {
+			setBounds(bounds);
+		} else {
+			GuiHelper.centerScreen(this);
+		}
+
+		addWindowListener(new WindowPreferencesSaverOnClose(applicationName));
+		setVisible(true);
 	}
 }
--- trunk/java/src/hu/pagavcs/gui/platform/GuiHelper.java
+++ trunk/java/src/hu/pagavcs/gui/platform/GuiHelper.java
@@ -8,6 +8,8 @@ import java.awt.Rectangle;
 import java.awt.Toolkit;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -158,10 +160,20 @@ public class GuiHelper {
 		}
 	}
 
-	private static void centerScreen(Window window) {
+	public static void centerScreen(Window window) {
 		Dimension dim = window.getToolkit().getScreenSize();
 		Rectangle bounds = window.getBounds();
 		window.setLocation((dim.width - bounds.width) / 2, (dim.height - bounds.height) / 2);
+	}
+
+	public static Frame createFrame(JComponent pnlMain, String applicationName, String iconName) {
+		if (iconName == null) {
+			iconName = "/hu/pagavcs/resources/icon.png";
+		}
+		Frame frame = new Frame(applicationName, iconName);
+		frame.getContentPane().add(addBorder(pnlMain));
+		frame.setTitle(applicationName + " [" + Manager.getApplicationRootName() + "]");
+		return frame;
 	}
 
 	public static Frame createAndShowFrame(JComponent pnlMain, String applicationName) {
@@ -169,12 +181,7 @@ public class GuiHelper {
 	}
 
 	public static Frame createAndShowFrame(JComponent pnlMain, String applicationName, String iconName) {
-		if (iconName == null) {
-			iconName = "/hu/pagavcs/resources/icon.png";
-		}
-		Frame frame = new Frame(iconName);
-		frame.getContentPane().add(addBorder(pnlMain));
-		frame.setTitle(applicationName + " [" + Manager.getApplicationRootName() + "]");
+		Frame frame = createFrame(pnlMain, applicationName, iconName);
 		frame.pack();
 
 		Rectangle bounds = Manager.getSettings().getWindowBounds(applicationName);
@@ -191,11 +198,18 @@ public class GuiHelper {
 	}
 
 	public static JDialog createDialog(Window parent, JComponent main, String title) {
-		JDialog dialog = new JDialog(parent);
+		final JDialog dialog = new JDialog(parent);
 		dialog.getContentPane().add(addBorder(main));
 		dialog.setTitle(title);
 		dialog.setIconImage(Toolkit.getDefaultToolkit().getImage(Manager.class.getResource("/hu/pagavcs/resources/icon.png")));
 		dialog.pack();
+
+		dialog.getRootPane().registerKeyboardAction(new ActionListener() {
+
+			public void actionPerformed(ActionEvent actionEvent) {
+				dialog.setVisible(false);
+			}
+		}, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);
 
 		Rectangle bounds = Manager.getSettings().getWindowBounds(title);
 		if (bounds != null) {
