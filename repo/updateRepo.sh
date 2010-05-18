9117ba70003ed0358181d204c738e892d1e8c987
Gábor Pápai <gabor.papai@gmail.com>
2010-05-18 06:39:16 +0000

    make own repository

---
+++ repo/updateRepo.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+reprepro -b . includedeb karmic ../trunk/*.deb
+reprepro -b . includedeb karmic ../trunk-mugcommander/*.deb
+reprepro -b . includedeb karmic ../trunk-mugcommander-gnome-extension/*.deb
+reprepro -b . includedeb karmic ../trunk-mugcommander-pagavcs-extension/*.deb
+reprepro -b . includedeb karmic ../trunk-mugcommnader-bonjour-extension/*.deb
