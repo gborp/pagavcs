e7f956cc7eb069f2aad6b4c2c024ce39b6e03e83
gabor.papai <gabor.papai@d68d8996-001d-11df-8113-359535dc4dea>
2010-06-02 14:09:22 +0000

    fix updateRepo.sh script

--- repo/updateRepo.sh
+++ repo/updateRepo.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
-reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk/*.deb
-reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommander/*.deb
-reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommander-gnome-extension/*.deb
-reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommander-pagavcs-extension/*.deb
-reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommnader-bonjour-extension/*.deb
+ls -1 ../trunk/*.deb > /dev/null 2>&1
+if [ "$?" = "0" ]; then
+	reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk/*.deb
+fi
+
+ls -1 ../trunk-mugcommander/*.deb > /dev/null 2>&1
+if [ "$?" = "0" ]; then
+	reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommander/*.deb
+fi
+
+ls -1 ../trunk-mugcommander-gnome-extension/*.deb > /dev/null 2>&1
+if [ "$?" = "0" ]; then
+	reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommander-gnome-extension/*.deb
+fi
+
+ls -1 ../trunk-mugcommander-pagavcs-extension/*.deb > /dev/null 2>&1
+if [ "$?" = "0" ]; then
+	reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommander-pagavcs-extension/*.deb
+fi
+
+ls -1 ../trunk-mugcommnader-bonjour-extension/*.deb > /dev/null 2>&1
+if [ "$?" = "0" ]; then
+	reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommnader-bonjour-extension/*.deb
+fi
