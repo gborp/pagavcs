1eb586ca0b27784b533c14575a51195c19e6ae02
gabor.papai <gabor.papai@d68d8996-001d-11df-8113-359535dc4dea>
2010-08-01 08:15:03 +0000

    pagavcs 1.4.30 and force asking passphrase in updateRepo.sh

--- repo/updateRepo.sh
+++ repo/updateRepo.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 ls -1 ../trunk/*.deb > /dev/null 2>&1
 if [ "$?" = "0" ]; then
-	reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk/*.deb
+	reprepro --ask-passphrase --keepunreferencedfiles -b . includedeb karmic ../trunk/*.deb
 fi
 
 ls -1 ../trunk-mugcommander/*.deb > /dev/null 2>&1
 if [ "$?" = "0" ]; then
-	reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommander/*.deb
+	reprepro --ask-passphrase --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommander/*.deb
 fi
 
 ls -1 ../trunk-mugcommander-gnome-extension/*.deb > /dev/null 2>&1
 if [ "$?" = "0" ]; then
-	reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommander-gnome-extension/*.deb
+	reprepro --ask-passphrase --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommander-gnome-extension/*.deb
 fi
 
 ls -1 ../trunk-mugcommander-pagavcs-extension/*.deb > /dev/null 2>&1
 if [ "$?" = "0" ]; then
-	reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommander-pagavcs-extension/*.deb
+	reprepro --ask-passphrase --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommander-pagavcs-extension/*.deb
 fi
 
 ls -1 ../trunk-mugcommnader-bonjour-extension/*.deb > /dev/null 2>&1
 if [ "$?" = "0" ]; then
-	reprepro --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommnader-bonjour-extension/*.deb
+	reprepro --ask-passphrase --keepunreferencedfiles -b . includedeb karmic ../trunk-mugcommnader-bonjour-extension/*.deb
 fi
