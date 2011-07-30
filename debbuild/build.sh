7ca92580d07b2c51d0374e9919ccd86d6f6b7b61
Gábor Pápai <gabor.papai@gmail.com>
2011-07-30 21:03:23 +0000

    Issue 132: Support Gnome 3 (and so Ubuntu 11.10)

--- debbuild/build.sh
+++ debbuild/build.sh
@@ -7,6 +7,7 @@ rmdir debian/input
 sudo rm -r debian/pagavcs/*
 sudo rmdir debian/pagavcs
 mkdir debian/input
+mkdir debian/input/gnome3
 mkdir debian/input/doc
 mkdir -p debian/input/icons/hicolor/scalable/actions
 mkdir -p debian/input/icons/hicolor/scalable/apps
@@ -15,6 +16,7 @@ cp ../java/dist/pagavcs.jar debian/input/
 cp ../java/dist/icon.png debian/input/
 cp ../scripts/pagavcs debian/input/
 cp ../scripts/pagavcs-nautilus.py debian/input/
+cp ../scripts/gnome3/pagavcs-nautilus.py debian/input/gnome3
 cp ../doc/* debian/input/doc
 cp ../icons/hicolor/scalable/actions/* debian/input/icons/hicolor/scalable/actions
 cp ../icons/hicolor/scalable/apps/* debian/input/icons/hicolor/scalable/apps
