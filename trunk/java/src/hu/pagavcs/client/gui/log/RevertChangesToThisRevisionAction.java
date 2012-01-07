5efe2d1de2a62ab918fe02450986bc01de1e217a
Gábor Pápai <gabor.papai@gmail.com>
2012-01-07 14:44:01 +0000

    bug #0 refactoring

---
+++ trunk/java/src/hu/pagavcs/client/gui/log/RevertChangesToThisRevisionAction.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package hu.pagavcs.client.gui.log;
+
+import hu.pagavcs.client.bl.ThreadAction;
+
+import java.awt.event.ActionEvent;
+
+class RevertChangesToThisRevisionAction extends ThreadAction {
+
+	private final LogGui logGui;
+
+	public RevertChangesToThisRevisionAction(LogGui logGui) {
+		super("Revert changes to this revision");
+		this.logGui = logGui;
+	}
+
+	public void actionProcess(ActionEvent e) throws Exception {
+		long rev = this.logGui.getSelectedLogItems().get(0).getRevision();
+		logGui.revertChangesToThisRevisionExact((rev + 1) + "-working");
+	}
+}
---
+++ trunk/java/src/hu/pagavcs/client/operation/BlameOperation.java
@@ -0,0 +1,144 @@
+package hu.pagavcs.client.operation;
+
+import hu.pagavcs.client.bl.Cancelable;
+import hu.pagavcs.client.bl.Manager;
+import hu.pagavcs.client.bl.PagaException;
+import hu.pagavcs.client.bl.SvnHelper;
+import hu.pagavcs.client.gui.BlameGui;
+import hu.pagavcs.client.gui.BlameListItem;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.prefs.BackingStoreException;
+
+import org.tmatesoft.svn.core.SVNCancelException;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.wc.ISVNAnnotateHandler;
+import org.tmatesoft.svn.core.wc.ISVNEventHandler;
+import org.tmatesoft.svn.core.wc.SVNClientManager;
+import org.tmatesoft.svn.core.wc.SVNEvent;
+import org.tmatesoft.svn.core.wc.SVNInfo;
+import org.tmatesoft.svn.core.wc.SVNLogClient;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+import org.tmatesoft.svn.core.wc.SVNWCClient;
+
+/**
+ * PagaVCS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.<br>
+ * <br>
+ * PagaVCS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.<br>
+ * <br>
+ * You should have received a copy of the GNU General Public License along with
+ * PagaVCS; If not, see http://www.gnu.org/licenses/.
+ */
+public class BlameOperation implements Cancelable {
+
+	private String   path;
+	private BlameGui gui;
+	private boolean  cancel;
+
+	public BlameOperation(String path) throws BackingStoreException, SVNException {
+		this.path = path;
+	}
+
+	public void execute() throws SVNException, BackingStoreException, PagaException {
+		gui = new BlameGui(this);
+		gui.display();
+		gui.setFile(path);
+		gui.setStatus(GeneralStatus.INIT);
+		File wcFile = new File(path);
+		SVNClientManager mgrSvn = Manager.getSVNClientManager(new File(path));
+		SVNWCClient wcClient = mgrSvn.getWCClient();
+		gui.setStatus(GeneralStatus.START);
+		try {
+			SVNInfo svnInfo = wcClient.doInfo(wcFile, SVNRevision.WORKING);
+
+			gui.setURL(svnInfo.getURL().toDecodedString());
+
+			gui.setStatus(GeneralStatus.COMPLETED);
+		} catch (SVNException ex) {
+			Manager.handle(ex);
+			gui.setStatus(GeneralStatus.FAILED);
+		} finally {
+			mgrSvn.dispose();
+		}
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public List<BlameListItem> doBlame(String path, String revision) throws SVNException, PagaException {
+		revision = revision.trim();
+		SVNRevision blameRevision;
+		if (revision.isEmpty()) {
+			blameRevision = SVNRevision.HEAD;
+		} else {
+			blameRevision = SVNRevision.create(Long.valueOf(revision));
+		}
+
+		SVNClientManager clientMgr = Manager.getSVNClientManager(new File(path));
+		try {
+			SVNLogClient logClient = clientMgr.getLogClient();
+			logClient.setEventHandler(new ISVNEventHandler() {
+
+				public void checkCancelled() throws SVNCancelException {
+					if (isCancel()) {
+						throw new SVNCancelException();
+					}
+				}
+
+				public void handleEvent(SVNEvent event, double progress) throws SVNException {}
+			});
+			final ArrayList<BlameListItem> lstBlame = new ArrayList<BlameListItem>();
+			logClient.doAnnotate(new File(path), SVNRevision.HEAD, SVNRevision.create(1), blameRevision, new ISVNAnnotateHandler() {
+
+				public void handleEOF() {}
+
+				public void handleLine(Date date, long revision, String author, String line, Date mergedDate, long mergedRevision, String mergedAuthor,
+				        String mergedPath, int lineNumber) throws SVNException {
+					BlameListItem li = new BlameListItem();
+					li.setDate(date);
+					li.setRevision(revision);
+					li.setAuthor(author);
+					// convert tabulators to 4 spaces
+					line = line.replace("\t", "    ");
+					li.setLine(line);
+					li.setLineNumber(lstBlame.size() + 1);
+					lstBlame.add(li);
+				}
+
+				public void handleLine(Date date, long revision, String author, String line) throws SVNException {
+					throw new RuntimeException("Not implemented");
+				}
+
+				public boolean handleRevision(Date date, long revision, String author, File contents) throws SVNException {
+					return false;
+				}
+
+			});
+
+			return lstBlame;
+		} finally {
+			clientMgr.dispose();
+		}
+	}
+
+	public void storeUrlForHistory(String url) {
+		SvnHelper.storeUrlForHistory(url);
+	}
+
+	public void setCancel(boolean cancel) {
+		this.cancel = cancel;
+	}
+
+	public boolean isCancel() {
+		return cancel;
+	}
+}
