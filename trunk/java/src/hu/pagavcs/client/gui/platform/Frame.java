dbd60c63ea20b80fb9aaf5c2b353a3f6cc75c7bf
Gábor Pápai <gabor.papai@gmail.com>
2012-01-08 18:08:45 +0000

    Issue 119:	Window sizes are not correct (small) --- resolve-conflict-gui; fix saving window bounds when closing by ESC

--- trunk/java/src/hu/pagavcs/client/gui/platform/Frame.java
+++ trunk/java/src/hu/pagavcs/client/gui/platform/Frame.java
@@ -8,6 +8,7 @@ import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -38,12 +39,16 @@ public class Frame extends JFrame {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setIconImage(ResourceBundleAccessor.getImage(iconName).getImage());
 
-		getRootPane().registerKeyboardAction(new ActionListener() {
+		getRootPane().registerKeyboardAction(
+				new ActionListener() {
 
-			public void actionPerformed(ActionEvent actionEvent) {
-				setVisible(false);
-			}
-		}, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+					public void actionPerformed(ActionEvent actionEvent) {
+						WindowEvent we = new WindowEvent(Frame.this,
+								WindowEvent.WINDOW_CLOSING);
+						processWindowEvent(we);
+						dispose();
+					}
+				}, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
 				JComponent.WHEN_IN_FOCUSED_WINDOW);
 	}
 
