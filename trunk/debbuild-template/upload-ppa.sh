57e710d418f43985709f6be57867c8509fd657e5
Gábor Pápai <gabor.papai@gmail.com>
2012-01-14 13:32:17 +0000

    [deb] new DEB package building system

--- trunk/debbuild-template/upload-ppa.sh
+++ trunk/debbuild-template/upload-ppa.sh
@@ -5,6 +5,10 @@ VERSION=`cat debian/changelog | head -1 | grep -o "[0-9]*\.[0-9]*\.[0-9]*\-[0-9]
 WDIRNAMESHORT=${APPNAME}_${VERSION}
 WDIRNAME=${WDIRNAMESHORT}~all
 
+./common.sh
+cd $WDIR
+debuild -S -sa
+
 cd ..
 #cp ${WDIRNAMESHORT}_source.changes ${WDIRNAME}_source.changes 
 dput ppa:gaborgabor/pagavcs ${WDIRNAME}_source.changes 
