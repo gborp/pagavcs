52412a9849627d6d15f8bfbee15c6feb0433228a
gabor.papai <gabor.papai@d68d8996-001d-11df-8113-359535dc4dea>
2012-01-14 13:20:38 +0000

    [deb] new DEB package building system

---
+++ trunk/debbuild-template/upload-ppa.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+APPNAME=pagavcs
+VERSION=`cat debian/changelog | head -1 | grep -o "[0-9]*\.[0-9]*\.[0-9]*\-[0-9]*"`
+WDIRNAMESHORT=${APPNAME}_${VERSION}
+WDIRNAME=${WDIRNAMESHORT}~all
+
+cd ..
+#cp ${WDIRNAMESHORT}_source.changes ${WDIRNAME}_source.changes 
+dput ppa:gaborgabor/pagavcs ${WDIRNAME}_source.changes 
