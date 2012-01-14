c9f317a275b461c606dd990ca8af8d9b2c23fe8d
Gábor Pápai <gabor.papai@gmail.com>
2012-01-14 13:20:38 +0000

    [deb] new DEB package building system

---
+++ debbuild-template/build.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+APPNAME=pagavcs
+VERSION=`cat debian/changelog | head -1 | grep -o "[0-9]*\.[0-9]*\.[0-9]*\-[0-9]*"`
+WDIRNAME=$APPNAME-${VERSION}
+WDIR=../$WDIRNAME
+
+rm -r $WDIR
+rmdir $WDIR
+mkdir $WDIR
+
+cd ../java
+./build.sh
+cd ../debbuild-template
+
+cp -r debian $WDIR/debian
+
+mkdir $WDIR/debian/input
+mkdir $WDIR/debian/input/gnome3
+mkdir $WDIR/debian/input/doc
+mkdir -p $WDIR/debian/input/icons/hicolor/scalable/actions
+mkdir -p $WDIR/debian/input/icons/hicolor/scalable/apps
+mkdir -p $WDIR/debian/input/icons/hicolor/scalable/emblems
+cp ../java/dist/pagavcs.jar $WDIR/debian/input/
+cp ../java/dist/icon.png $WDIR/debian/input/
+cp ../scripts/pagavcs $WDIR/debian/input/
+cp ../scripts/pagavcs-nautilus.py $WDIR/debian/input/
+cp ../scripts/gnome3/pagavcs-nautilus.py $WDIR/debian/input/gnome3
+cp ../doc/* $WDIR/debian/input/doc
+cp ../icons/hicolor/scalable/actions/* $WDIR/debian/input/icons/hicolor/scalable/actions
+#cp ../icons/hicolor/scalable/apps/*    $WDIR/debian/input/icons/hicolor/scalable/apps
+cp ../icons/hicolor/scalable/emblems/* $WDIR/debian/input/icons/hicolor/scalable/emblems
+
+cd $WDIR
+debuild -sa
+debuild -S -sa
+
+
+rm -r $WDIR
+rmdir $WDIR
+
+#user=`whoami`
+#sudo chown -R $user *
