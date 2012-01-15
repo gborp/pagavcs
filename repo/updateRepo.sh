e87025796ecbd774aeb8af3a0049b52cbe33ff0c
Gábor Pápai <gabor.papai@gmail.com>
2012-01-15 09:18:12 +0000

    [deb] new DEB package building system

--- repo/updateRepo.sh
+++ repo/updateRepo.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
-ls -1 ../trunk/*.deb > /dev/null 2>&1
-if [ "$?" = "0" ]; then
-	reprepro --ask-passphrase --keepunreferencedfiles -b . includedeb karmic ../trunk/*.deb
-fi
+#!/bin/bash
+
+function update_pagavcs {
+	ls -1 ../trunk/debbuild-temp/*$1*.deb > /dev/null 2>&1
+	if [ "$?" = "0" ]; then
+		reprepro --ask-passphrase --keepunreferencedfiles -b . includedeb $1 ../trunk/debbuild-temp/*$1*.deb
+	fi
+}
+
+update_pagavcs karmic
+update_pagavcs lucid
+update_pagavcs maverick
+update_pagavcs natty
+update_pagavcs oneiric
+
 
 ls -1 ../trunk-mugcommander/*.deb > /dev/null 2>&1
 if [ "$?" = "0" ]; then
