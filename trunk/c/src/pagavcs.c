169d8fe665d4958b9eaf5e0e9f49693ff170c20f
gabor.papai <gabor.papai@d68d8996-001d-11df-8113-359535dc4dea>
2012-02-01 18:47:45 +0000

    cli: if path argument is not present use the current directory

--- trunk/c/src/pagavcs.c
+++ trunk/c/src/pagavcs.c
@@ -57,35 +57,37 @@ int main(int argc, char *argv[]) {
 	char fullLogFilename[512];
 	char argument[512];
 	char argumentToUse[512];
+	char *newArgv[argc + 1];
 
-	if (argc == 1 || strcmp(argv[1], "help") == 0
-			|| strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "-h") == 0
-			|| strcmp(argv[1], "-?") == 0) {
+if(	argc == 1 || strcmp(argv[1], "help") == 0
+	|| strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "-h") == 0
+	|| strcmp(argv[1], "-?") == 0) {
 		printf("usage: pagavcs [command] [argument(s)]\n");
-		printf("  update         Update working copy.\n");
-		printf("  commit         Show commit dialog for working copy.\n");
-		printf("  log            Show log for working copy.\n");
-		printf("  ignore         Add file or directory to ignore list.\n");
-		printf("  unignore       Remove file or directory from ignore list.\n");
-		printf("  delete         Delete file or directory.\n");
-		printf("  revert         Revert file or directory.\n");
-		printf("  checkout       Checkout a working copy.\n");
-		printf("  cleanup        Cleanup working copy.\n");
-		printf("  resolve        Resolve conflict.\n");
-		printf("  other          Display other window.\n");
-		printf("  settings       Display settings window.\n");
-		printf("  stop           Stop PagaVCS server.\n");
+		printf("  update           Update working copy.\n");
+		printf("  commit           Show commit dialog for working copy.\n");
+		printf("  log              Show log for working copy.\n");
+		printf("  ignore           Add file or directory to ignore list.\n");
 		printf(
-				"  getmenuitems   (For internal use.) Get menuitems for file or directory.\n");
-		printf("  -w, -wait      Wait until command finished\n");
+		"  unignore         Remove file or directory from ignore list.\n");
+		printf("  delete           Delete file or directory.\n");
+		printf("  revert           Revert file or directory.\n");
+		printf("  checkout         Checkout a working copy.\n");
+		printf("  cleanup          Cleanup working copy.\n");
+		printf("  resolve          Resolve conflict.\n");
+		printf("  other            Display other window.\n");
+		printf("  settings         Display settings window.\n");
+		printf("  stop, exit, quit Stop PagaVCS server.\n");
 		printf(
-				"  -c, -autoclose Automatically close window after finished sucessfully\n");
+		"  getmenuitems     (For internal use.) Get menuitems for file or directory.\n");
+		printf("  -w, -wait        Wait until command finished\n");
+		printf(
+		"  -c, -autoclose   Automatically close window after finished sucessfully\n");
 		printf("  help, h\n");
-		printf("  -h, --help     Display this help.\n");
+		printf("  -h, --help       Display this help.\n");
 		printf("\n");
 		printf("Example:\n");
 		printf(
-				"  pagavcs update \"/home/johnny/big-project\" \"/home/johnny/mega-project\"\n");
+		"  pagavcs update \"/home/johnny/big-project\" \"/home/johnny/mega-project\"\n");
 		return 0;
 	}
 
@@ -112,6 +114,32 @@ int main(int argc, char *argv[]) {
 	strcpy(address.sun_path, fullSocketFilename);
 
 	connectServer(socket_fd, address, fullLogFilename);
+
+	int foundNonOptions = 0;
+	int argPhase = 0;
+	argIndex = 1;
+	while (argIndex < argc) {
+		strcpy(argument, argv[argIndex]);
+
+		if (argPhase == 0) {
+			if (argument[0] != '-') {
+				argPhase = 1;
+			}
+		}
+		if (argPhase == 1) {
+			foundNonOptions++;
+		}
+		argIndex++;
+	}
+	if (foundNonOptions < 2) {
+
+		for (argIndex = 0; argIndex < argc; argIndex++) {
+			newArgv[argIndex] = argv[argIndex];
+		}
+		newArgv[argc] = ".";
+		argv = newArgv;
+		argc++;
+	}
 
 	for (argIndex = 1; argIndex < argc; argIndex++) {
 		write(socket_fd, "\"", strlen("\""));
