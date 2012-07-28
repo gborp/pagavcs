29c50f899fcd7156ae31e6fb585db9a758a4c99f
Gábor Pápai <gabor.papai@gmail.com>
2012-07-28 17:07:27 +0000

    more verbose "connection error" on network timeout

--- java/src/hu/pagavcs/client/bl/PagaException.java
+++ java/src/hu/pagavcs/client/bl/PagaException.java
@@ -3,6 +3,7 @@ package hu.pagavcs.client.bl;
 public class PagaException extends Exception {
 
 	private final PagaExceptionType type;
+	private String verboseMessage;
 
 	public enum PagaExceptionType {
 		LOGIN_FAILED, CONNECTION_ERROR, UNIMPLEMENTED, INVALID_PARAMETERS, NOT_DIRECTORY
@@ -11,11 +12,20 @@ public class PagaException extends Exception {
 	public PagaException(PagaExceptionType type) {
 		super(type.toString());
 		this.type = type;
+	}
 
+	public PagaException(PagaExceptionType type, String verboseMessage) {
+		super(type.toString() + " " + verboseMessage);
+		this.type = type;
+		this.verboseMessage = verboseMessage;
 	}
 
 	public PagaExceptionType getType() {
 		return type;
+	}
+
+	public String getVerboseMessage() {
+		return verboseMessage;
 	}
 
 }
