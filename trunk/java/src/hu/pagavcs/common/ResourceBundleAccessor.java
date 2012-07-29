7bac7a5f2b5af70276f566d8165c9f6c35c45baa
Gábor Pápai <gabor.papai@gmail.com>
2012-07-29 18:07:43 +0000

    [repo browser] icons in context menu

--- trunk/java/src/hu/pagavcs/common/ResourceBundleAccessor.java
+++ trunk/java/src/hu/pagavcs/common/ResourceBundleAccessor.java
@@ -1,11 +1,22 @@
 package hu.pagavcs.common;
 
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 import java.net.URL;
 
 import javax.swing.ImageIcon;
 
 public class ResourceBundleAccessor {
+
+	public static ImageIcon getSmallImage(String name) {
+		ImageIcon image = getImage(name);
+		BufferedImage result = new BufferedImage(image.getIconWidth(),
+				image.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+		image.paintIcon(null, result.getGraphics(), 0, 0);
+
+		return new ImageIcon(result.getScaledInstance(16, 16,
+				BufferedImage.SCALE_SMOOTH));
+	}
 
 	public static ImageIcon getImage(String name) {
 		URL url = ResourceBundleAccessor.class
