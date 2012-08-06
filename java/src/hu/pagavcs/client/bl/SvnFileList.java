92d140620f73ea3b11a831f3222b93c52ea91ffb
Gábor Pápai <gabor.papai@gmail.com>
2012-08-06 18:59:55 +0000

    [log] fix exception on showing log on a file

--- java/src/hu/pagavcs/client/bl/SvnFileList.java
+++ java/src/hu/pagavcs/client/bl/SvnFileList.java
@@ -7,44 +7,33 @@ import java.util.List;
 import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.SVNURL;
 import org.tmatesoft.svn.core.internal.util.SVNEncodingUtil;
-import org.tmatesoft.svn.core.internal.util.SVNURLUtil;
 import org.tmatesoft.svn.core.wc.SVNClientManager;
 import org.tmatesoft.svn.core.wc.SVNInfo;
 import org.tmatesoft.svn.core.wc.SVNRevision;
 
 public class SvnFileList {
 
-	private SVNURL   baseURL;
+	private SVNURL baseURL;
 	private String[] target;
 
 	public SvnFileList(File... files) throws SVNException {
 		List<SVNURL> lstInfo = new ArrayList<SVNURL>();
-		SVNClientManager mgrSvn = Manager.getSVNClientManagerForWorkingCopyOnly();
+		SVNClientManager mgrSvn = Manager
+				.getSVNClientManagerForWorkingCopyOnly();
 		try {
 			for (File file : files) {
-				SVNInfo svnInfo = mgrSvn.getWCClient().doInfo(file, SVNRevision.WORKING);
+				SVNInfo svnInfo = mgrSvn.getWCClient().doInfo(file,
+						SVNRevision.WORKING);
+				baseURL = svnInfo.getRepositoryRootURL();
 				lstInfo.add(svnInfo.getURL());
 			}
 		} finally {
 			mgrSvn.dispose();
 		}
 
-		String[] paths = new String[lstInfo.size()];
+		target = new String[lstInfo.size()];
 		for (int i = 0; i < lstInfo.size(); i++) {
-			paths[i] = lstInfo.get(i).getPath();
-		}
-
-		// String rootWCPath = SVNPathUtil.condencePaths(paths, null, true);
-		ArrayList<String> targetPaths = new ArrayList<String>();
-		baseURL = SVNURLUtil.condenceURLs(lstInfo.toArray(new SVNURL[0]), targetPaths, true);
-		if (targetPaths.isEmpty()) {
-			targetPaths.add("");
-		}
-
-		target = new String[targetPaths.size()];
-
-		for (int i = 0; i < target.length; i++) {
-			target[i] = SVNEncodingUtil.uriDecode(targetPaths.get(i));
+			target[i] = SVNEncodingUtil.uriDecode(lstInfo.get(i).getPath());
 		}
 	}
 
