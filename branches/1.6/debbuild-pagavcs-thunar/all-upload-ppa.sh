659c2395a851d270cd79a4e0eb93875cd7954ae3
gabor.papai <gabor.papai@d68d8996-001d-11df-8113-359535dc4dea>
2012-10-17 21:10:10 +0000

    project reorg

--- branches/1.6/debbuild-pagavcs-thunar/all-upload-ppa.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm -f -r ../temp-build
-
-for dist in oneiric precise
-do
-	./upload-ppa.sh $dist
-done
--- branches/1.6/java/src/hu/pagavcs/client/gui/log/LogDetailCopyToClipboardType.java
+++
@@ -1,5 +0,0 @@
-package hu.pagavcs.client.gui.log;
-
-public enum LogDetailCopyToClipboardType {
-	DETAIL, GROUP_NAME_ONLY
-}
