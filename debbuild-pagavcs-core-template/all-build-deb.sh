8a4d5cd1a0e51f2269f001086351e0bede7230cf
Gábor Pápai <gabor.papai@gmail.com>
2012-12-02 18:52:54 +0000

    moving MugCommander to separate branch

--- debbuild-pagavcs-core-template/all-build-deb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -f -r ../temp-build
-
-for dist in karmic lucid maverick natty oneiric
-do
-	./build-deb.sh $dist
-done
-
--- mugcommander/java/source/hu/pagavcs/mug/findfile/FindProcessCallback.java
+++
@@ -1,9 +0,0 @@
-package hu.pagavcs.mug.findfile;
-
-
-public interface FindProcessCallback {
-
-	void actualDir(String dirName);
-
-	void actualFile(String fileName);
-}
