d92fcb63bb99a562439cf02242aa72c9e6fe854c
Gábor Pápai <gabor.papai@gmail.com>
2012-12-02 17:59:35 +0000

    moving MugCommander to separate branch

--- mugcommander/java/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ant
-cp source/com/mucommander/res/images/mucommander/icon48_24.png dist/mugcommander-icon.png
--- mugcommander/java/source/com/mucommander/extensions/ServiceExtension.java
+++
@@ -1,9 +0,0 @@
-package com.mucommander.extensions;
-
-import com.mucommander.ui.main.SplashScreen;
-
-public interface ServiceExtension {
-
-	void startService(boolean useSplash, SplashScreen splashScreen);
-
-}
