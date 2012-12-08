5d27b47e3dccc7cc273c6649496e1190b0be0c52
Gábor Pápai <gabor.papai@gmail.com>
2012-12-08 05:50:06 +0000

    1.7

--- branches/1.7/debbuild-pagavcs-bigmem/common.sh
+++ branches/1.7/debbuild-pagavcs-bigmem/common.sh
@@ -8,7 +8,7 @@ fi
 OWNDIR=`pwd`
 DIST=$1
 
-APPNAME=pagavcs-bigmem
+APPNAME=pagavcs-17-bigmem
 COMMONBUILDDIR=temp-build
 VERSION=`cat debian/changelog | head -1 | grep -o "[0-9]*\.[0-9]*\.[0-9]*\-[0-9]*"`
 WDIRNAME=${APPNAME}_${VERSION}${DIST}
