44bcb8e2651981de58d1bf569e7d0c09e6e6f29a
gabor.papai <gabor.papai@d68d8996-001d-11df-8113-359535dc4dea>
2012-12-26 18:57:16 +0000

    backport from 1.7.2  to 1.6.8

--- branches/1.6/debbuild-pagavcs-core-template/common.sh
+++ branches/1.6/debbuild-pagavcs-core-template/common.sh
@@ -37,9 +37,7 @@ mkdir -p $WDIR/debian/input/icons/hicolor/scalable/apps
 mkdir -p $WDIR/debian/input/icons/hicolor/scalable/emblems
 cp ../java/dist/pagavcs.jar $WDIR/debian/input/
 cp ../java/dist/icon.png $WDIR/debian/input/
-cp ../scripts/pagavcs $WDIR/debian/input/
 cp ../doc/* $WDIR/debian/input/doc
 cp ../icons/hicolor/scalable/actions/* $WDIR/debian/input/icons/hicolor/scalable/actions
-#cp ../icons/hicolor/scalable/apps/*    $WDIR/debian/input/icons/hicolor/scalable/apps
 cp ../icons/hicolor/scalable/emblems/* $WDIR/debian/input/icons/hicolor/scalable/emblems
 
