9d8af5ffce5854adf79cb8df189b7254e21a2873
Gábor Pápai <gabor.papai@gmail.com>
2012-12-29 17:18:42 +0000

    merging from 1.7: Issue 161 (repo browser) add copy/delete operation fix cli argument passing

--- java/src/hu/pagavcs/client/gui/platform/MessagePane.java
+++ java/src/hu/pagavcs/client/gui/platform/MessagePane.java
@@ -4,6 +4,7 @@ import hu.pagavcs.client.bl.Manager;
 import hu.pagavcs.client.bl.OnSwing;
 import hu.pagavcs.client.bl.ThreadAction;
 
+import java.awt.Component;
 import java.awt.Dialog.ModalityType;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
@@ -12,6 +13,7 @@ import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
@@ -21,34 +23,59 @@ import com.jgoodies.forms.layout.FormLayout;
 public class MessagePane {
 
 	public enum OPTIONS {
-		OK, CANCEL
+		OK, NO, CANCEL
 	}
 
-	protected final Window       parent;
-	protected final String       title;
-	protected final String       message;
-	protected OPTIONS            optionChoosed;
-	protected JDialog            dialog;
-	protected final Icon         icon;
+	protected final Window parent;
+	protected final String title;
+	protected final String message;
+	protected OPTIONS optionChoosed;
+	protected JDialog dialog;
+	protected final Icon icon;
 	protected final ModalityType modalityType;
 
 	public static OPTIONS showError(Window parent, String title, String message) {
-		return execute(parent, Manager.getIconError(), title, message, ModalityType.DOCUMENT_MODAL);
+		return execute(parent, Manager.getIconError(), title, message,
+				ModalityType.DOCUMENT_MODAL);
 	}
 
-	public static OPTIONS showWarning(Window parent, String title, String message) {
-		return execute(parent, Manager.getIconWarning(), title, message, ModalityType.DOCUMENT_MODAL);
+	public static OPTIONS showWarning(Window parent, String title,
+			String message) {
+		return execute(parent, Manager.getIconWarning(), title, message,
+				ModalityType.DOCUMENT_MODAL);
 	}
 
 	public static OPTIONS showInfo(Window parent, String title, String message) {
-		return execute(parent, Manager.getIconInformation(), title, message, ModalityType.DOCUMENT_MODAL);
+		return execute(parent, Manager.getIconInformation(), title, message,
+				ModalityType.DOCUMENT_MODAL);
 	}
 
-	protected static OPTIONS execute(Window parent, Icon icon, String title, String message, ModalityType modalityType) {
-		return new MessagePane(parent, icon, title, message, modalityType).execute();
+	protected static OPTIONS execute(Window parent, Icon icon, String title,
+			String message, ModalityType modalityType) {
+		return new MessagePane(parent, icon, title, message, modalityType)
+				.execute();
 	}
 
-	public MessagePane(Window parent, Icon icon, String title, String message, ModalityType modalityType) {
+	public static OPTIONS executeConfirmDialog(Component parent, String message) {
+		int answer = JOptionPane.showConfirmDialog(parent, message);
+		if (answer == JOptionPane.CANCEL_OPTION) {
+			return OPTIONS.CANCEL;
+		} else {
+			if (answer == JOptionPane.YES_OPTION) {
+				return OPTIONS.OK;
+			}
+			return OPTIONS.NO;
+		}
+	}
+
+	public static String executeInputDialog(Component parent, String title,
+			String message) {
+		return JOptionPane.showInputDialog(parent, message, title,
+				JOptionPane.QUESTION_MESSAGE);
+	}
+
+	public MessagePane(Window parent, Icon icon, String title, String message,
+			ModalityType modalityType) {
 		this.parent = parent;
 		this.icon = icon;
 		this.title = title;
@@ -60,13 +87,15 @@ public class MessagePane {
 		JPanel panel = new JPanel(new FormLayout("p:g", "p:g,2dlu,p"));
 		CellConstraints cc = new CellConstraints();
 
-		Label lblMessage = new Label(StringHelper.convertMultilineTextToHtml(message));
+		Label lblMessage = new Label(
+				StringHelper.convertMultilineTextToHtml(message));
 		lblMessage.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMessage.setIcon(icon);
 		JButton btnOk = new JButton(new OkAction());
 
 		panel.add(lblMessage, cc.xywh(1, 1, 1, 1));
-		panel.add(btnOk, cc.xywh(1, 3, 1, 1, CellConstraints.CENTER, CellConstraints.DEFAULT));
+		panel.add(btnOk, cc.xywh(1, 3, 1, 1, CellConstraints.CENTER,
+				CellConstraints.DEFAULT));
 
 		return panel;
 	}
