f3c98845dcc744cc347a0274346df5af86e8c756
Gábor Pápai <gabor.papai@gmail.com>
2013-08-24 19:30:57 +0000

    pagavcs-17-core, -core-libs, -cli packages have been merged into pagavcs-17

--- debbuild-pagavcs-template/common.sh
+++ debbuild-pagavcs-template/common.sh
@@ -23,9 +23,27 @@ rm -r -f $WDIR
 mkdir $WDIR
 
 cd ../java
+./build.sh
 
 cd $OWNDIR
 
 cp -r debian $WDIR/debian
 
 sed -i "s/karmic/$DIST/g" $WDIR/debian/changelog
+
+cp -R ../c/* $WDIR
+
+mkdir $WDIR/debian/input
+mkdir $WDIR/debian/input/doc
+mkdir -p $WDIR/debian/input/icons/hicolor/scalable/actions
+mkdir -p $WDIR/debian/input/icons/hicolor/scalable/apps
+mkdir -p $WDIR/debian/input/icons/hicolor/scalable/emblems
+cp ../java/dist/pagavcs.jar $WDIR/debian/input/
+cp ../java/dist/pagavcs-libs.jar $WDIR/debian/input/
+cp ../java/dist/icon.png $WDIR/debian/input/
+cp ../doc/* $WDIR/debian/input/doc
+cp ../icons/hicolor/scalable/actions/* $WDIR/debian/input/icons/hicolor/scalable/actions
+cp ../icons/hicolor/scalable/emblems/* $WDIR/debian/input/icons/hicolor/scalable/emblems
+
+
+
